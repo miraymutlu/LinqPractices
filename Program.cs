d3660832094b181e85de61db75f8c55603908f0f
Miray Mutlu <miray.mutlu@ozu.edu.tr>
2023-08-26 16:14:54 +0300

    singleordefault and firstordefault added

--- Program.cs
+++ Program.cs
@@ -10,10 +10,22 @@ class Program
         LinqDBContext _context = new LinqDBContext();
         var students = _context.Students.ToList<Student>();
         
-        //find()
+        //Find()
         Console.WriteLine("*** Find ***");
         var student = _context.Students.Where(student => student.StudentId == 1).FirstOrDefault();
         var student2 = _context.Students.Find(2);
-        Console.WriteLine($"Student 1: {student.Name} \n"+$"Student 2: {student2.Name}");
+        Console.WriteLine($"Student 1: {student.Name} \n"+$"Student 2: {student2.Name}\n");
+        
+        //FirstOrDefault()
+        Console.WriteLine("*** First Or Default ***");
+        student = _context.Students.Where(student => student.Surname == "Mutlu").FirstOrDefault();
+        Console.WriteLine($"Student 1: {student.Name}\n");
+        
+        //SingleOrDefault()
+        Console.WriteLine("*** Single Or Default ***");
+        student = _context.Students.Where(student => student.Surname == "Ataç").SingleOrDefault();
+        Console.WriteLine($"Student 1: {student.Name}\n");
+
+
     }
 }
